a7265031a64dddd8a1ff7ff1a9e5c447c7fb838c
HHR86 <116777280+HHR86@users.noreply.github.com>
2023-07-23 00:20:03 -0300

    carpeta Backend
    se agrega carpeta Backend

---
+++ ProyectoFinal/BACKEND/src/main/java/com/ProyectoARP/HHR/HhrApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoARP.HHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhrApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal/BACKEND/src/test/java/com/ProyectoARP/HHR/HhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoARP.HHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
